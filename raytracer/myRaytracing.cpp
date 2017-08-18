d3ed94cf70a4c4a297df0a773d050290d1ceb2e3
Rouder <1158261439@qq.com>
2017-08-18 12:19:35 +0800

    The first edition!  Version 1.0

--- raytracer/myRaytracing.cpp
+++ raytracer/myRaytracing.cpp
@@ -38,7 +38,7 @@ public:
 	}
 	Vec3<T> operator * (const T &f) const { return Vec3<T>(x*f, y*f, z*f); }
 	friend Vec3<T> operator * (const T &f, const Vec3<T> &v) { return v*f; }
-	Vec3<T> operator * (const Vec3<T> &v) const { return Vec3<T>(x*v.x, y*v.y, z*v.z); }	//逐分量相乘
+	Vec3<T> operator * (const Vec3<T> &v) const { return Vec3<T>(x*v.x, y*v.y, z*v.z); }	//逐分量相乘，光线与材料混合
 	T dot(const Vec3<T> &v) const { return x*v.x + y*v.y + z*v.z; }
 	Vec3<T> operator - (const Vec3<T> &v) const { return Vec3<T>(x - v.x, y - v.y, z - v.z); }
 	Vec3<T> operator + (const Vec3<T> &v) const { return Vec3<T>(x + v.x, y + v.y, z + v.z); }
@@ -61,8 +61,12 @@ class Sphere
 public:
 	Vec3f center;
 	float radius, radius2;
-	Vec3f surfaceColor, emissionColor;		//自发光颜色
-	float transparency, reflection;			//还可以加上一个折射率属性
+	Vec3f surfaceColor;				//漫反射颜色，几乎所有外来光线都要与这个颜色交互融合
+	Vec3f emissionColor;			//自发光颜色，物体表层的一层光线，光线的方向都是法线的方向
+	float transparency;				//透明度
+	float reflection;				//镜面反射度
+	//还可以加上一个折射率属性
+
 	Sphere(
 		const Vec3f &c,
 		const float &r,
@@ -73,9 +77,9 @@ public:
 		center(c), radius(r), radius2(r*r), surfaceColor(sc), emissionColor(ec), transparency(transp), reflection(refl)
 	{}
 
-	//计算点center到直线raydir的距离d，如果d小于等于radius，那么就相交(rayorig可能在一个圆的内部)
-	//t0和t1用于接收相交点到oig的距离：如果ray的发射点在球体内部，t1是击中距离；如果ray的发射点在球体外部，t0是击中距离
-	bool intersect(const Vec3f &rayorig, const Vec3f &raydir, float &t0, float &t1)const	//这里设置两个float似乎是多余的,迟点改写这个函数。。。。。。
+	//计算点center到直线raydir的距离d，如果d小于等于radius，那么就相交；
+	//最后要返回是否相交，还要通过引用返回相交点与rayorig的距离(注意rayorig可能在物体内部，也可能在物体外部)
+	bool intersect(const Vec3f &rayorig, const Vec3f &raydir, float &t)const
 	{
 		Vec3f l = center - rayorig;
 		float projectOflToRaydir = l.dot(raydir);
@@ -85,22 +89,24 @@ public:
 		if (d2 > radius2)
 			return false;
 		float offset = sqrt(radius2 - d2);
-		t0 = projectOflToRaydir - offset;
-		t1 = projectOflToRaydir + offset;
+		float t0 = projectOflToRaydir - offset;
+		float t1 = projectOflToRaydir + offset;
+		
+		t = t0 < 0 ? t1 : t0;		//根据发射点在不在物体内部来确定击中距离
 		return true;
 	}
 };
 
 #define MAX_RAY_DEPTH 5		//递归深度
 
-//这是什么混合方法
+//Fresnel equation
 float mix(const float &a, const float &b, const float &mix)
 {
 	return b*mix + a*(1 - mix);
 }
 
-//有问题。。。。。。。。。。。。。。。。。
 //raydir一定要是单位向量
+//所有的光线都要相加(叠加)，但是如果有光线L与物体表面是交互的，那么光线L在叠加之前要逐分量相乘(混合)
 Vec3f trace(
 	const Vec3f &rayorig,
 	const Vec3f &raydir,
@@ -111,27 +117,26 @@ Vec3f trace(
 	const Sphere* nearSphere = NULL;		//ray击中的最靠近的球体
 	for (unsigned i = 0; i < spheres.size(); ++i)
 	{
-		float t0 = INFINITY, t1 = INFINITY;
-		if (spheres[i].intersect(rayorig, raydir, t0, t1))
+		float t = INFINITY;
+		if (spheres[i].intersect(rayorig, raydir, t))
 		{
-			float hitFaceDist = t0 < 0/*rayorig在物体内部*/ ? t1 : t0;
-			if (hitFaceDist < minHitDist)
+			if (t < minHitDist)
 			{
-				minHitDist = hitFaceDist;
+				minHitDist = t;
 				nearSphere = &spheres[i];
 			}
 		}
 	}
 
 	if (nearSphere == NULL)
-		return Vec3f(2);		//射线打不中球体，返回背景色rgb(2*255,2*255,2*255)
+		return Vec3f(2);							//射线打不中球体，返回背景色rgb(2*255,2*255,2*255)
 
 	//计算ray击中点的数据
 	Vec3f surfaceColor = 0;
 	Vec3f phit = rayorig + raydir*minHitDist;		//ray击中点
 	Vec3f nhit = phit - nearSphere->center;			//ray击中点的球体外法线
 	nhit.normalize();
-	bool inside = false;	//ray的发射点是不是在内部
+	bool inside = false;							//ray的发射点是不是在内部
 	if (raydir.dot(nhit) > 0)
 	{
 		nhit = -nhit;
@@ -139,47 +144,49 @@ Vec3f trace(
 	}
 
 	//开始计算ray击中点的颜色
-	float bias = 1e-4;		//误差值
-	//难道 漫反射 和 镜面反射+折射 冲突吗？？？？
+	float bias = 1e-4;		//误差值，为什么设置为0会折射会出问题？？？？？
+
+	//难道 漫反射 和 镜面反射+折射 是互斥的吗？？？？
 	if ((nearSphere->transparency > 0 || nearSphere->reflection > 0) && depth < MAX_RAY_DEPTH)
 	{
-		//计算镜面反射效果，不判断就直接计算，是因为只要是折射都会有反射吗？？？？
-		Vec3f refldir = raydir - nhit * 2 * raydir.dot(nhit);	//反射方向
-		refldir.normalize();	//似乎没有必要归一化，因为入射向量是单位向量
+		//下面计算反射与折射的过程都是比较简单的，因为我们只考虑一条反射线和一条折射线
+
+		//计算镜面反射效果，不判断就直接计算，是因为只要是折射都会有镜面反射吗？？？？
+		Vec3f refldir = raydir - nhit * 2 * raydir.dot(nhit);
+		//refldir.normalize();
 		//递归
-		Vec3f reflection = trace(phit + nhit*bias, refldir, spheres, depth + 1);	//这里让shadow ray的发射点产生偏差，如果不做偏差会怎么样？。。。。。。
+		Vec3f reflection = trace(phit + nhit*bias, refldir, spheres, depth + 1);	//这里让shadow ray的发射点产生偏差
 
 		//计算折射效果
 		Vec3f refraction;
 		if (nearSphere->transparency > 0)
 		{
 			//计算折射方向
-			float ior = 1.1;	//这里统一所有的球体内部的折射率为1.1
-			float niCmpT0nt = (inside) ? ior : (1 / ior);	//这个变量就是变量n，n=ni/nt
+			float ior = 1.1;														//这里统一所有的球体内部的折射率为1.1
+			float niCmpT0nt = (inside) ? ior : (1 / ior);							//这个变量就是变量n，n=ni/nt
 			float cosi = -nhit.dot(raydir);
 			float cost = sqrt(1 - niCmpT0nt*niCmpT0nt*(1 - cosi*cosi));
 			Vec3f refrdir = (niCmpT0nt*cosi - cost)*nhit + niCmpT0nt*raydir;
-			refldir.normalize();
+			//refldir.normalize();
 			//递归
-			refraction = trace(phit - nhit*bias, refrdir, spheres, depth + 1);		//这里让shadow ray的发射点产生偏差，但是为什么这里是减，上面是加？？？
+			refraction = trace(phit - nhit*bias, refrdir, spheres, depth + 1);		//这里让shadow ray的发射点产生偏差
 		}
 
-		//计算出 菲涅尔 混合值
+		//计算出 菲涅尔 混合值，确定折射光与反射光的叠加比例
 		float facingration = -raydir.dot(nhit);
 		float fresneleffect = mix(pow(1 - facingration, 3), 1, 0.1);
-		//怎么混合的？？？
-		surfaceColor = (reflection*fresneleffect +						/*为什么反射效果不要乘上 nearSphere->reflection ???*/
+
+		surfaceColor = (reflection*fresneleffect +									/*为什么反射效果不要乘上 nearSphere->reflection ???*/
 			refraction*(1 - fresneleffect)*nearSphere->transparency)
 			*nearSphere->surfaceColor;
-			
 	}
 	else
 	{
 		//为什么漫反射不用递归？？？
+		//这里我们假设：漫反射材质只从其它发光球体获取光线；这个假设明显有问题，会导致阴影不正常
 		for (unsigned i = 0; i < spheres.size(); ++i)
 		{
-			//这里应该要将自己排除在外，因为自己不能接收自己的光线
-			if (spheres[i].emissionColor.x > 0)			//这里我们假设：漫反射材质只从其它发光球体获取光线；这个假设明显有问题
+			if (&spheres[i]!=nearSphere && spheres[i].emissionColor.x > 0)			//排除自己，寻找发光体			
 			{
 				//检查有没有挡住光线
 				bool block = false;
@@ -190,12 +197,12 @@ Vec3f trace(
 				{
 					if(j==i)		//排除发光体
 						continue;
-					float t0, t1;
-					if (spheres[j].intersect(phit + nhit*bias, lightDire, t0, t1))
+					float t;
+					if (spheres[j].intersect(phit + nhit*bias, lightDire, t))
 						break;
 				}
 				if (j == spheres.size())	//没物体挡住光线
-					surfaceColor += std::max(float(0), nhit.dot(lightDire))*spheres[i].emissionColor*nearSphere->surfaceColor;		//怎么混合的？？
+					surfaceColor += std::max(float(0), nhit.dot(lightDire))*spheres[i].emissionColor*nearSphere->surfaceColor;
 			}
 		}
 	}
@@ -210,9 +217,6 @@ void render(const std::vector<Sphere> &spheres,const unsigned &pixelNumOfWidth,c
 	Vec3f *image = new Vec3f[pixelNumOfWidth*pixelNumOfHeight];
 	Vec3f *pixel = image;
 
-	///////////////////////////////////////////////////////////////////////////////
-	//射线计算有问题。。。。。。。。。。。。。但是影响不大，迟点再做调整
-
 	//确定每一个像素的位置，然后确定primary ray的方向
 	//这里我们假定cavas这个平面位于z=-1这个平面上
 	float aspectratio = pixelNumOfWidth / (float)pixelNumOfHeight;
@@ -220,7 +224,7 @@ void render(const std::vector<Sphere> &spheres,const unsigned &pixelNumOfWidth,c
 	float width = height * aspectratio;					//canvas高度
 	float widthOfPixel = width / pixelNumOfWidth;		//canvas上的像素宽度
 	float heightOfPixel = height / pixelNumOfHeight;	//canvas上的像素高度
-	//
+	//初始坐标
 	float x0 = -(width / 2) + (widthOfPixel / 2);
 	float y0 = (height / 2) - (heightOfPixel / 2);
 	//确定每个像素的颜色，这个循环可以并行计算
@@ -236,16 +240,15 @@ void render(const std::vector<Sphere> &spheres,const unsigned &pixelNumOfWidth,c
 			(*pixel++) = trace(Vec3f(0), primaryRayDir, spheres, 0);	//第一个参数可以是像素点的位置
 		}
 	}
-	///////////////////////////////////////////////////////////////////////////////
 
 	//输出到PPM文件
 	std::ofstream ofs("./untitled.ppm", std::ios::out | std::ios::binary);
 	ofs << "P6\n" << pixelNumOfWidth << " " << pixelNumOfHeight << "\n255\n";
 	for (unsigned i = 0; i < pixelNumOfWidth*pixelNumOfHeight; ++i)
 	{
-		ofs << (unsigned char)(std::min((float)1, image[i].x) * 255)
-			<< (unsigned char)(std::min((float)1, image[i].y) * 255)
-			<< (unsigned char)(std::min((float)1, image[i].z) * 255);	//不做四舍五入了？？？
+		ofs << (unsigned char)(std::min((float)1, image[i].x) * 255 + 0.5)
+			<< (unsigned char)(std::min((float)1, image[i].y) * 255 + 0.5)
+			<< (unsigned char)(std::min((float)1, image[i].z) * 255 + 0.5);
 	}
 	ofs.close();
 	delete[] image;
@@ -253,19 +256,18 @@ void render(const std::vector<Sphere> &spheres,const unsigned &pixelNumOfWidth,c
 
 int main()
 {
-	srand(13);
+	srand(13);	//干什么的？？？
 	std::vector<Sphere> spheres;
 	//object
 	spheres.push_back(Sphere(Vec3f(0.0, -10004, -20), 10000, Vec3f(0.20, 0.20, 0.20), 0, 0, 0.0));
 	spheres.push_back(Sphere(Vec3f(0.0, 0, -20), 4, Vec3f(1.00, 0.32, 0.36), 1, 0.5));
-	spheres.push_back(Sphere(Vec3f(-3.5, 3.0, -30), 4, Vec3f(0.00, 1.00, 0.00), 0, 0));
-	spheres.push_back(Sphere(Vec3f(5.0, -1, -15), 2, Vec3f(0.90, 0.76, 0.46), 1, 0.0));
+	spheres.push_back(Sphere(Vec3f(-5.0, 3.0, -30), 4, Vec3f(0.36, 0.80, 0.36), 0, 0));
+	spheres.push_back(Sphere(Vec3f(6.0, -1, -15), 2, Vec3f(0.90, 0.76, 0.46), 1, 0.0));
 	spheres.push_back(Sphere(Vec3f(5.0, 0, -25), 3, Vec3f(0.65, 0.77, 0.97), 1, 0.0));
-	spheres.push_back(Sphere(Vec3f(-5.5, 0, -15), 3, Vec3f(0.90, 0.90, 0.90), 1, 0.0));
+	spheres.push_back(Sphere(Vec3f(-7.5, 0, -15), 3, Vec3f(0.90, 0.90, 0.90), 1, 0.0));
 	//light
 	spheres.push_back(Sphere(Vec3f(0.0, 20, -30), 3, Vec3f(0.00, 0.00, 0.00), 0, 0.0, Vec3f(3)));
-	render(spheres, 1280, 720, 60);
-	//render(spheres);
+	render(spheres, 1920, 1080, 60);
 	return 0;
 }
 
