c634235982f946953eec4cc96b26091e58c78093
Rouder <1158261439@qq.com>
2018-03-03 19:29:04 +0800

    ready to create a camera

--- raytracer/myRaytracing.cpp
+++ raytracer/myRaytracing.cpp
@@ -21,9 +21,11 @@ float fresnelMix(const float &a, const float &b, const float &mix)
 	return b*mix + a*(1 - mix);
 }
 
+//迟点将下面两个参数作为render函数的参数
 //这里使用的环境光并不考虑遮挡效果
-#define AMBIENT Vec3f(0.05, 0.05, 0.05)
+#define AMBIENT Vec3f(0.3, 0.3, 0.3)		//环境光是一种trick
 #define BACKGROUP_COLOR Vec3f(2)
+
 //raydir一定要是单位向量
 //所有的光线都要相加(叠加)，但是如果有光线L与物体表面是交互的，那么光线L在叠加之前要逐分量相乘(混合)
 Vec3f trace(
@@ -31,7 +33,7 @@ Vec3f trace(
 	const Vec3f &raydir,
 	const std::vector<Sphere> &spheres,
 	const int &depth)
-{
+{ 
 	float minHitDist = INFINITY;			//rayorig到最靠近的平面的距离
 	const Sphere* nearSphere = NULL;		//ray击中的最靠近的球体
 	for (unsigned i = 0; i < spheres.size(); ++i)
@@ -47,13 +49,13 @@ Vec3f trace(
 		}
 	}
 
+	//射线打不中球体，返回背景光rgb(2*255,2*255,2*255)
 	if (nearSphere == NULL)
-		return BACKGROUP_COLOR + AMBIENT;							//射线打不中球体，返回背景色rgb(2*255,2*255,2*255)
+		return BACKGROUP_COLOR;
 
 	//计算ray击中点的数据
-	Vec3f color = 0;
+	
 	Vec3f phit = rayorig + raydir*minHitDist;		//ray击中点
-
 	Vec3f nhit;
 	bool inside;
 	Vec2f uv;
@@ -62,48 +64,49 @@ Vec3f trace(
 	//开始计算ray击中点的颜色
 	float bias = 1e-4;		//偏移值
 
-	//难道 漫反射 和 镜面反射+折射 是互斥的吗？不应该是互斥的，将下面的else去掉就好
+	Vec3f color = 0;
 	if ((nearSphere->transparency > 0 || nearSphere->reflection > 0) && depth < MAX_RAY_DEPTH)
 	{
 		//下面计算反射与折射的过程都是比较简单的，因为我们只考虑一条反射线和一条折射线
 
-		//计算镜面反射效果，不判断就直接计算，是因为只要是折射都会有镜面反射吗？？？？
+		//完全镜面反射
+		//只要是折射都会有镜面反射
 		Vec3f refldir = raydir - nhit * 2 * raydir.dot(nhit);
-		//refldir.normalize();
-		//递归
-		Vec3f reflection = trace(phit + nhit*bias, refldir, spheres, depth + 1);	//这里让shadow ray的发射点产生偏差
+		refldir.normalize();
+		Vec3f reflection = trace(phit + nhit*bias, refldir, spheres, depth + 1);
 
-		//计算折射效果
+		//折射
 		Vec3f refraction;
 		if (nearSphere->transparency > 0)
 		{
-			//计算折射方向
+			//折射方向
 			float ior = 1.1;														//这里统一所有的球体内部的折射率为1.1
 			float niCmpT0nt = (inside) ? ior : (1 / ior);							//这个变量就是变量n，n=ni/nt
 			float cosi = -nhit.dot(raydir);
 			float cost = sqrt(1 - niCmpT0nt*niCmpT0nt*(1 - cosi*cosi));
 			Vec3f refrdir = (niCmpT0nt*cosi - cost)*nhit + niCmpT0nt*raydir;
-			//refldir.normalize();
-			//递归
-			refraction = trace(phit - nhit*bias, refrdir, spheres, depth + 1);		//这里让shadow ray的发射点产生偏差
+			refldir.normalize();
+
+			refraction = trace(phit - nhit*bias, refrdir, spheres, depth + 1);
 		}
 
 		//计算出 菲涅尔 混合值，确定折射光与反射光的叠加比例
 		float facingration = -raydir.dot(nhit);
 		float fresneleffect = fresnelMix(pow(1 - facingration, 3), 1, 0.1);
 
-		color = (
+		color += (
 				reflection*fresneleffect +									/*为什么反射效果不要乘上 nearSphere->reflection ???*/
-				refraction*(1 - fresneleffect)*nearSphere->transparency + 
-				AMBIENT
+				refraction*(1 - fresneleffect)*nearSphere->transparency
 				)
 				*nearSphere->getSurfaceColor(uv);
 	}
 	else
 	{
+		//表面非常粗糙时，镜面反射就近似于下面这种漫反射
+
+		color += AMBIENT * nearSphere->getSurfaceColor(uv);
 		//为什么漫反射不用递归？？？
 		//这里我们假设：漫反射材质只从其它发光球体获取光线；这个假设明显有问题，会导致阴影不正常
-		color += AMBIENT * nearSphere->getSurfaceColor(uv);
 		for (unsigned i = 0; i < spheres.size(); ++i)
 		{
 			if (&spheres[i]!=nearSphere && spheres[i].emissionColor.x > 0)			//排除自己，寻找发光体			
@@ -131,7 +134,7 @@ Vec3f trace(
 }
 
 
-
+//#define JITTERING
 //fov是上下夹角
 void render(const std::vector<Sphere> &spheres,const unsigned &pixelNumOfWidth,const unsigned &pixelNumOfHeight,const float &fov)
 {
@@ -146,22 +149,36 @@ void render(const std::vector<Sphere> &spheres,const unsigned &pixelNumOfWidth,c
 	float width = height * aspectratio;					//canvas高度
 	float widthOfPixel = width / pixelNumOfWidth;		//canvas上的像素宽度
 	float heightOfPixel = height / pixelNumOfHeight;	//canvas上的像素高度
-	//初始坐标
+
+#ifdef JITTERING
+
+#else
+	//第一个像素中心点的摄像机坐标
 	float x0 = -(width / 2) + (widthOfPixel / 2);
 	float y0 = (height / 2) - (heightOfPixel / 2);
+#endif
+
 	//确定每个像素的颜色，这个循环可以并行计算
 	//注意：PPM文件必须按照从上到下、从左到右的顺序输入一幅图像的信息
 	for (int i = 0; i < pixelNumOfHeight; ++i)			//行
-	{
 		for (int j = 0; j < pixelNumOfWidth; ++j)		//列
 		{
+#ifdef JITTERING
+			int n = 4;
+			for (int p = 0; p < n; ++p)
+				for (int q = 0; q < n; q++)
+				{
+
+				}
+#else
+			//不带反走样
 			float pixelX = x0 + j*widthOfPixel;
 			float pixelY = y0 - i*heightOfPixel;
 			Vec3f primaryRayDir(pixelX, pixelY, -1);
 			primaryRayDir.normalize();
 			(*pixel++) = trace(Vec3f(0), primaryRayDir, spheres, 0);	//第一个参数可以是像素点的位置
+#endif
 		}
-	}
 
 	out2PPM(image, pixelNumOfWidth, pixelNumOfHeight, "untitled.ppm");
 
@@ -209,8 +226,8 @@ int main()
 	std::vector<Sphere> spheres;
 	//object
 	spheres.push_back(Sphere(Vec3f(0.0, -10004, -20), 10000, Vec3f(0.8, 0.5, 0.15), NULL, 0, 0, 0.0));
-	spheres.push_back(Sphere(Vec3f(0.0, 0, -20), 4, Vec3f(1.00, 0.32, 0.36), NULL, 1, 0.5));
-	spheres.push_back(Sphere(Vec3f(0.0, 0, -20), 5, Vec3f(0.f), textureFunc2, 1));		//盖住了上一个球，测试完纹理就删掉
+	spheres.push_back(Sphere(Vec3f(0.0, 0, -20), 4, Vec3f(1.00, 0.32, 0.36), NULL, 0, 0.5));
+	//spheres.push_back(Sphere(Vec3f(0.0, 0, -20), 5, Vec3f(0.f), textureFunc2, 1));		//盖住了上一个球，测试完纹理就删掉
 	spheres.push_back(Sphere(Vec3f(-5.0, 3.0, -30), 4, Vec3f(0.36, 0.80, 0.36), NULL, 0, 0));
 	spheres.push_back(Sphere(Vec3f(6.0, -1, -15), 2, Vec3f(0.90, 0.76, 0.46), NULL, 1, 0.0));
 	spheres.push_back(Sphere(Vec3f(5.0, 0, -25), 3, Vec3f(0.65, 0.77, 0.97), NULL, 1, 0.0));
